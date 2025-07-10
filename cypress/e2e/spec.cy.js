21816c0e0c5c90bc1c34b12b7527ceca0397835a
Dadio1719 <damilola.adio1719@gmail.com>
2025-07-10 13:21:59 +0100

    new update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe("ZojaTech Assessment", () => {
+describe("Assessment", () => {
   it("Verify user can launch the App", () => {
     cy.visit("https://parabank.parasoft.com/parabank/index.htm")
     cy.get('input[value="Log In"]').should('be.visible')
